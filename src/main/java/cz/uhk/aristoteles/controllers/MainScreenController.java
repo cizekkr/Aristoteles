b9411ca53636efc88b0b70dd0bb674e5bc905f79
Adam Černohorský <Adam ernohorsk@DESKTOP-BCD18VL>
2017-08-07 21:03:47 +0200

    druhý commit

--- src/main/java/cz/uhk/aristoteles/controllers/MainScreenController.java
+++ src/main/java/cz/uhk/aristoteles/controllers/MainScreenController.java
@@ -15,25 +15,45 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 
 public class MainScreenController implements Initializable {
-    
+
     @FXML
     private Button btnShowAllRooms;
-    
+
     @FXML
     private AnchorPane aPaneRoomPane;
-    
+
     @FXML
-    private void showAllRooms(ActionEvent event) throws IOException{
+    private void showAllRooms(ActionEvent event) throws IOException {
         List<Room> rooms = Session.getDatabase().getAllRooms();
-        for (int i = 0; i < rooms.size(); i++){
+        for (int i = 0; i < rooms.size(); i++) {
             AnchorPane room = FXMLLoader.load(getClass().getResource("/fxml/RoomPreview.fxml"));
             aPaneRoomPane.getChildren().add(room);
+            setRoomPreviewLayout(room, i);
         }
     }
-    
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
+
+    private void setRoomPreviewLayout(AnchorPane room, int roomNumberInList) {
+        int roomPaneWidth = (int) aPaneRoomPane.getWidth();
+        int roomPreviewHeight = (int) room.getPrefHeight();
+        int roomPreviewWidth = (int) room.getPrefWidth();
+        int period = roomPaneWidth / roomPreviewWidth;
+
+        int row = roomNumberInList / period;
+        int column = roomNumberInList % period;
+
+        int paddingX = (roomPaneWidth % roomPreviewWidth) / (period + 1) * (roomNumberInList % period + 1);
+        int paddingY = (roomPaneWidth % roomPreviewWidth) / (period + 1) * (roomNumberInList / period + 1);
+
+        int offSetX = column * roomPreviewWidth + paddingX;
+        int offSetY = row * roomPreviewHeight + paddingY;
+
+        room.setTranslateX(offSetX);
+        room.setTranslateY(offSetY);
+    }
+
 }
